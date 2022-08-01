6b79aff8f4d8383192364fe838cad6c8517df592
Argha Nath <54589605+arghanath007@users.noreply.github.com>
2022-08-01 12:27:34 +0530

    updated helper function

--- Deep Learning/Transfer Learning/helper_functions.py
+++ Deep Learning/Transfer Learning/helper_functions.py
@@ -5,7 +5,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-%matplotlib inline
+# %matplotlib inline
 
 from tensorflow import keras
 from tensorflow.keras import layers
